b145e2678afca0f5c3f9b0892dbd8a7137fce70c
Spicy-Material <53911304+Spicy-Material@users.noreply.github.com>
2019-10-18 08:39:58 +0530

    Added Biggest Element In An Array

---
+++ Biggest element in an array.c
@@ -0,0 +1,22 @@
+// Find the biggest array element
+//Spicy-Material
+#include<stdio.h>
+int main()
+{
+	int a[i]
+	printf("Enter no. of datas:")
+	scanf("%d",&b); 	
+	int max=0;
+	max=a[0];
+
+	for(i=0;i<b;i++)
+	{
+		printf("\n");
+		if(a[i]>max)
+		{
+			max=a[i];
+		}
+	}
+	printf("\n Biggest =%d",max);
+	return 0;
+}
